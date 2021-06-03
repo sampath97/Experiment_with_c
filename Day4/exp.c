10150c08d599a6022148f418ebf0e5e0c27dbd36
sampathrachumallu <sampath.r@spovum.com>
2021-06-03 19:48:07 +0530

    *added code for day4 *started reading pointers ,so less code today

--- Day4/exp.c
+++ Day4/exp.c
@@ -1,10 +1,16 @@
+
 #include<stdio.h>
-int get_length(int arr[]);
 
 int main()
 {
-	int a[3]={1,2,3,4};
-	printf("%d",a[3]);
+  int a[5]={1},b[5]={1};
+  if(a==b)
+  {
+  	printf("same");
+  }
+  else
+  {
+  	printf("Different");
+  }
+  
 }
-
-
